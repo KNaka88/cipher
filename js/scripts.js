224185fbac27df2965c1485537391c8f1ed77257
Charles Peden and Koji Nakagawa <charles@>
2017-01-09 16:55:20 -0800

    add semi-functional replace statement

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var userSentence = "i like pie.";
+alert(userSentence);
+//var newuserSentence = userSentence.replace(/.^/, userSentence.charAt[0].toUpperCase());
+var newuserSentence = userSentence.replace(/^./, "a");
+alert(newuserSentence);
+/*
+var str = 'Twas the night before Xmas...';
+var newstr = str.replace(/xmas/i, 'Christmas');
+alert(newstr);
+*/
